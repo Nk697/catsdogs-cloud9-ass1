538a903e43a0f85c0467eef129e69f2308a154bf
EC2 Default User <ec2-user@ip-172-31-23-72.us-east-2.compute.internal>
2022-06-06 18:47:29 +0000

    Added terraform code and gitignore file

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Nobel"
+    "App"   = "Web"
+    "Project" = "Ass1"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "npkass1"
+}
